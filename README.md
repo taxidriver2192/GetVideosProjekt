259c40a0e630e5ab3b16038dae232f4f8ec78131
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-04-08 14:05:40 +0200

    Test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # GetVideosProjekt
+Til at downloade Videoer for internettet, vil jeg vælger at bruge goLang, som funger ligesom Pthon, men bare mere optimeret og lætter at læse.
--- main.go
+++ main.go
@@ -7,22 +7,57 @@ import (
 	"os"
 )
 
-type logWriter struct{}
-
 func main() {
-	resp, err := http.Get("http://google.com")
+	// Instantiate default collector
+	c := colly.NewCollector(
+		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
+		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
+	)
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// Print link
+		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// Visit link found on page
+		// Only those links are visited which are in AllowedDomains
+		c.Visit(e.Request.AbsoluteURL(link))
+	})
+
+	// Before making a request print "Visiting ..."
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL.String())
+	})
+
+	// Start scraping on https://hackerspaces.org
+	c.Visit("https://hackerspaces.org/")
+
+	fileUrl := "http://micl-easj.dk/Machine%20Learning/Noter%20and%20Books/Machine%20Learning%20Landscape%20Ch.%201/ML%20Landscape%20No.%201%20Overview%20%202021.01.28.mp4"
+	err := DownloadFile("Test.mp4", fileUrl)
 	if err != nil {
-		fmt.Println("Error:", err)
-		os.Exit(1)
+		panic(err)
 	}
-
-	lw := logWriter{}
-
-	io.Copy(lw, resp.Body)
+	fmt.Println("Downloaded: " + fileUrl)
 }
 
-func (logWriter) Write(bs []byte) (int, error) {
-	fmt.Println(string(bs))
-	fmt.Println("Just wrote this many bytes:", len(bs))
-	return len(bs), nil
+// DownloadFile will download a url to a local file. It's efficient because it will
+// write as it downloads and not load the whole file into memory.
+func DownloadFile(filepath string, url string) error {
+
+	// Get the data
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	// Create the file
+	out, err := os.Create(filepath)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
 }
