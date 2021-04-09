c5c96e6b68deb14a338a110ffaeb54535f53d717
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-04-09 16:19:24 +0200

    Update GET ALL LINKS

--- main.go
+++ main.go
@@ -1,63 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"net/http"
-	"os"
-)
-
 func main() {
-	// Instantiate default collector
-	c := colly.NewCollector(
-		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
-		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
-	)
 
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
-		// Visit link found on page
-		// Only those links are visited which are in AllowedDomains
-		c.Visit(e.Request.AbsoluteURL(link))
-	})
-
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
-
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
-
-	fileUrl := "http://micl-easj.dk/Machine%20Learning/Noter%20and%20Books/Machine%20Learning%20Landscape%20Ch.%201/ML%20Landscape%20No.%201%20Overview%20%202021.01.28.mp4"
-	err := DownloadFile("Test.mp4", fileUrl)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Downloaded: " + fileUrl)
+	// DOWNLOADE FILE IN MAIN FUNKTION!
+	// fileUrl := "http://micl-easj.dk/Machine%20Learning/Noter%20and%20Books/Machine%20Learning%20Landscape%20Ch.%201/ML%20Landscape%20No.%201%20Overview%20%202021.01.28.mp4"
+	// err := DownloadFile("Test.mp4", fileUrl)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println("Downloaded: " + fileUrl)
 }
 
 // DownloadFile will download a url to a local file. It's efficient because it will
 // write as it downloads and not load the whole file into memory.
-func DownloadFile(filepath string, url string) error {
+// func DownloadFile(filepath string, url string) error {
 
-	// Get the data
-	resp, err := http.Get(url)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
+// 	// Get the data
+// 	resp, err := http.Get(url)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer resp.Body.Close()
 
-	// Create the file
-	out, err := os.Create(filepath)
-	if err != nil {
-		return err
-	}
-	defer out.Close()
+// 	// Create the file
+// 	out, err := os.Create(filepath)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer out.Close()
 
-	// Write the body to file
-	_, err = io.Copy(out, resp.Body)
-	return err
-}
+// 	// Write the body to file
+// 	_, err = io.Copy(out, resp.Body)
+// 	return err
+// }
