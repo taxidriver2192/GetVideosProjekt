4b37c7c84cd4ed7bfa44ca3dfe7022a03b1d3ff9
Lukas Jürs Schmidt <mr.lukas.schmidt@gmail.com>
2021-04-11 21:29:21 +0200

    Push Har snart Struct til at virke.

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"log"
 	"net/url"
 	"os"
 	"regexp"
@@ -12,18 +10,12 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-type Any interface{}
-type autoInc_person struct {
+type autoInc struct {
 	sync.Mutex
 	id int
 }
 
-type autoInc_video struct {
-	sync.Mutex
-	id int
-}
-
-func (a *autoInc_person) ID_P() (id int) {
+func (a *autoInc) ID() (id int) {
 	a.Lock()
 	defer a.Unlock()
 	id = a.id
@@ -31,94 +23,47 @@ func (a *autoInc_person) ID_P() (id int) {
 	return
 }
 
-func (b *autoInc_video) ID_V() (id int) {
-	b.Lock()
-	defer b.Unlock()
-	id = b.id
-	b.id++
-	return
-}
-
-var ai_p autoInc_person
-var ai_v autoInc_video
-
-type Person struct {
-	ID_Person   int
-	PrimitiveID string
-	Navn        string
-	Website     string
-	Videos      []Video
+type Videos interface {
+	NewVideo()
 }
 type Video struct {
-	ID_Video int
-	Titel    string
-	Url      string
-	Dato     string
+	Id    int
+	Titel string
+	Url   string
+	Dato  string
+}
+type Person struct {
+	Id      int
+	Navn    string
+	Website string
+	Videos  []Video
 }
 
-type Videos []*Video
+var ai autoInc
 
-func (vs Videos) Process(f func(video *Video)) {
-	for _, video := range vs {
-		f(video)
-	}
-}
-
-func (vs Videos) FindAll(f func(video *Video) bool) Videos {
-	videos := make([]*Video, 0)
-	vs.Process(func(v *Video) {
-		if f(v) {
-			videos = append(videos, v)
-		}
-	})
-	return videos
-}
-
-func (vs Videos) Map(f func(video *Video) Any) []Any {
-	result := make([]Any, 0)
-	ix := 0
-	vs.Process(func(v *Video) {
-		result[ix] = f(v)
-		ix++
-	})
-	return result
-}
-
-func MakeSortedAppender(manufacture []string) (func(video *Video), map[string]Videos) {
-	sortedVideos := make(map[string]Videos)
-
-	for _, m := range manufacture {
-		sortedVideos[m] = make([]*Video, 0)
-	}
-
-	sortedVideos["Default"] = make([]*Video, 0)
-
-	appender := func(v *Video) {
-		if _, ok := sortedVideos[v.Dato]; ok {
-			sortedVideos[v.Dato] = append(sortedVideos[v.Dato], v)
-		} else {
-			sortedVideos["Default"] = append(sortedVideos["Default"], v)
-		}
-	}
-
-	return appender, sortedVideos
-}
-
-var PersonData Person
-var VideoData Video
-
-func NewPerson() *Person {
+func newPerson() *Person {
 	return &Person{
-		ID_Person: ai_p.ID_P(),
+		Id: ai.ID(),
 	}
 }
 func NewVideo() *Video {
 	return &Video{
-		ID_Video: ai_v.ID_V(),
+		Id: ai.ID(),
 	}
 }
 
+func (box *Person) AddItem(item Video) []Video {
+	box.Videos = append(box.Videos, item)
+	return box.Videos
+}
+
 func main() {
+	personData := Person{
+		Id:      newPerson().Id,
+		Navn:    "Micl",
+		Website: "test.dk",
+		Videos:  []Video{},
+	}
 
 	c := colly.NewCollector(
 		// Visit only domains: hackerspaces.org, wiki.hackerspaces.org
@@ -138,14 +83,10 @@ func main() {
 
 		if r_validVideoLink.MatchString(link) == true {
 			// info.ID = int(ai.ID())
-			tempVideoTitel := e.Text
-			tempVideoDato := ""
-			// Sådan at jeg kan se hvor mange den har fundet i terminalen.
-			u, TestError := url.Parse(link)
-			if TestError != nil {
-				log.Fatal(TestError)
-			}
-			tempVideoUrl := ("http://micl-easj.Dk" + u.EscapedPath())
+			videoTitel := e.Text
+			videoDato := ""
+			u, _ := url.Parse(link)
+			videoUrl := ("http://micl-easj.Dk" + u.EscapedPath())
 			if r_findDate.MatchString(link) == true {
 				dato := r_findDate.Copy().Find([]byte(link))
 				// Ved ikke hvorfor men nogle gange skriver han YYYY.MM.DD
@@ -155,40 +96,43 @@ func main() {
 					year := fmt.Sprint(string(dato[:+4]))
 					month := fmt.Sprint(string(dato[5:7]))
 					date := string(dato[len(dato)-2:])
-					tempVideoDato = (date + "." + month + "." + year)
+					videoDato = (date + "." + month + "." + year)
 				} else {
 					// Her har den fundet datoeen men den står ikke forkert.
-					tempVideoDato = string(dato)
+					videoDato = string(dato)
 				}
 			} else if OtherDatoField.MatchString(link) == true {
 				// Tester om datoen står på en anden måde?
 				dato := OtherDatoField.Copy().Find([]byte(link))
-				tempVideoDato = string(dato)
+				videoDato = string(dato)
 			} else {
 				// Her har den ikke fundet er dato. og sætter derfpr datoem til N/A
-				tempVideoDato = "N/A"
+				videoDato = "N/A"
 			}
-			test123 := ai_v.id
-			micl := &Video{test123, VideoData.Titel, tempVideoUrl, tempVideoDato}
-			allVideos := Videos([]*Video{micl})
-			allNewVideos := allVideos.FindAll(func(video *Video) bool {
-				return (video.ID_Video == 1) && (video.ID_Video > 2010)
-			})
-			fmt.Println("All Videos: ", allVideos)
-			fmt.Println("New Videos: ", allNewVideos)
+			//fmt.Println(personData)
+			hejVideo := Video{Id: NewVideo().Id, Titel: videoTitel, Url: videoUrl, Dato: videoDato}
+			hejVideo.print()
 
-			copyright := []string{"mich", "Person2", "Person3"}
-			sortedAppender, sortedVideos := MakeSortedAppender(copyright)
-			allVideos.Process(sortedAppender)
-			fmt.Println("Map sortedVideos: ", sortedVideos)
-			MichCount := len(sortedVideos["mich"])
-			fmt.Println("Mic har lavet ", MichCount, "Videoer")
-			VideoData.Titel = tempVideoTitel
-			VideoData.Url = tempVideoUrl
-			VideoData.Dato = tempVideoDato
+			personData.print()
+
+			//personData
+			// hej := person{
+			// 	id:      personData.id,
+			// 	navn:    personData.navn,
+			// 	website: personData.website,
+			// 	videos: []video{
+			// 		video{
+			// 			id:    videoData.id,
+			// 			titel: videoTitel,
+			// 			url:   videoUrl,
+			// 			dato:  videoDato,
+			// 		},
+			// 	},
+			// }
+
 			//fmt.Printf("Video_ID: %d -> Dato: %s Titel: %s\n", test123, VideoData.Dato, VideoData.Titel)
 
-			mydata, _ := json.MarshalIndent(NewVideo(), PersonData.PrimitiveID, PersonData.Navn)
+			//mydata, _ := json.MarshalIndent(NewVideo(), PersonData.PrimitiveID, PersonData.Navn)
 
 			f, err := os.OpenFile("myfile.data", os.O_APPEND|os.O_WRONLY, 0600)
 			if err != nil {
@@ -196,9 +140,9 @@ func main() {
 			}
 			defer f.Close()
 
-			if _, err = f.WriteString(string(mydata)); err != nil {
-				panic(err)
-			}
+			// if _, err = f.WriteString(string(mydata)); err != nil {
+			// 	panic(err)
+			// }
 
 		} else {
 			c.Visit(e.Request.AbsoluteURL("http://micl-easj.dk/" + link))
@@ -211,10 +155,17 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	NewPerson()
-	PersonData.Navn = "test"
-	PersonData.Website = "http://micl-easj.dk/"
+
+	// PersonData.Navn = "test"
+	// PersonData.Website = "http://micl-easj.dk/"
 
 	c.Visit("http://micl-easj.dk/")
 
+}
+
+func (p Person) print() {
+	fmt.Println(p)
+}
+func (v Video) print() {
+	fmt.Println(v)
 }
